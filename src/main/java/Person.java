45149c483529cafb400ebf787e3d34996f9b579e
PGowri <gowri.palaniswamy@gmail.com>
2024-02-12 22:51:20 -0500

    constructor part done

---
+++ src/main/java/Person.java
@@ -0,0 +1,26 @@
+package src.main.java;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+
+    public Person(){
+        this("name", null, null, null);
+    }
+    public Person(String name, LocalDate birthday, Sex gender, String emailAddress){
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.emailAddress = emailAddress;
+    }
+
+}
